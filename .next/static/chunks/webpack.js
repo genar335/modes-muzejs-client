c57d3012fa6cc01def763c50c2f1b322f6e37af0
Daniel <daniel.voronezhskiy@gmail.com>
2020-09-29 19:02:14 +0300

    success screen wip

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2182c0d231e5c223ab5b";
+/******/ 	var hotCurrentHash = "dfdfa98e62d30ca32a9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/client/success.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+function Success() {
+    
+    return(
+        <>
+            <Link href="/client/languages">Done</Link>
+        </>
+    );
+}
+
+export default Success
