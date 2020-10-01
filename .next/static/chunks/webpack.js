2d00a8aa83eb2a794fa539d41ceb4b43fd92db39
genar335 <domvlielupe3@gmail.com>
2020-10-01 12:56:21 +0300

    Added 'main' page for the tms started working on the carousel with tests

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "787cd65470ab5f5fa1bd";
+/******/ 	var hotCurrentHash = "8616bba20feca296ac7c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/TMS/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ActiveTests from "../../components/ActiveTests";
+import Gallery from "../../components/gallery";
+
+const main = () => {
+  return (
+    <div>
+      <ActiveTests />
+      <Gallery />
+    </div>
+  );
+};
+
+export default main;
