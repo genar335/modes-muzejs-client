4033d8d8a354f53f5ebfb93f0fb8e0fb7354f2ee
Daniel <daniel.voronezhskiy@gmail.com>
2020-10-13 16:13:00 +0300

    test wip

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "767091b41a18d27a285b";
+/******/ 	var hotCurrentHash = "069eb297fa404511bb43";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/client/test.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import Head from "next/head";
+
+function Test(props: any) {
+
+}
+
+export default Languages;
