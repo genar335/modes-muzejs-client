51ee90e18cb40f081e2d447b3950c6bc0fbd4e5d
genar335 <domvlielupe3@gmail.com>
2020-12-13 16:34:28 +0200

    Next statci files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "408f2b0a1e5bc132a80d";
+/******/ 	var hotCurrentHash = "9cf172f19fcbf8328336";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
