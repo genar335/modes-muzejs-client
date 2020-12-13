77bfb7c679629ffbe442e72ecb764c1c9efb27ed
genar335 <domvlielupe3@gmail.com>
2020-12-13 15:45:28 +0200

    Minor commments on test_selection

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "12a6bcdef0da8d15a2b1";
+/******/ 	var hotCurrentHash = "f5694c9c1cd5ddb8fc05";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/client/test_selection.tsx
+++ pages/client/test_selection.tsx
@@ -5,44 +5,43 @@ import TestCardClient from "../../components/TestCardClient";
 import styles from "../styles/test_selection.module.scss";
 
 function test_selection(props: any) {
+  const [testObjs, setTestObjs] = useState([]); //array with test objects
+  //! @insoemnia, you do not really need to store the url in the state, since you do not really change it.
+  const [url, setUrl] = useState(
+    "http://localhost:4000/tests/getTest?active=true"
+  ); //?dev url of active tests?
 
-    const [testObjs, setTestObjs] = useState([]); //array with test objects
-    const [url, setUrl] = useState("http://localhost:4000/tests/getTest?active=true"); //dev url of active tests?
+  const getActiveTests = (url: string) => {
+    Axios.get(url)
+      .then((res) => {
+        console.log(res);
+        setTestObjs(res.data);
+      })
+      .catch((error) => alert(error));
+  };
 
-    const getActiveTests = (url: string) => {
-        Axios.get(url)
-        .then(res => setTestObjs(res.data))
-        .catch(error => alert(error));
-    }
+  useEffect(() => {
+    getActiveTests(url);
+  });
 
-    useEffect(() => {
-        getActiveTests(url);
-    });
+  //TODO: direct to the chosen test
 
-    //TODO: direct to the chosen test
-
-    return(
-        <div className={styles.pageContainer}>
-            <img src="https://www.fashionmuseumriga.lv/bitrix/templates/main_template/img/logo.png" alt="logo" id={styles.MMlogo}/>
-            <div className={styles.galleryContainer}>
-                <TestCardClient 
-                    title="test1"
-                />
-                <TestCardClient 
-                    title="test2"
-                />
-                <TestCardClient 
-                    title="test3"
-                />
-                <TestCardClient 
-                    title="test4"
-                />
-                <TestCardClient 
-                    title="test5"
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.pageContainer}>
+      <img
+        src="https://www.fashionmuseumriga.lv/bitrix/templates/main_template/img/logo.png"
+        alt="logo"
+        id={styles.MMlogo}
+      />
+      <div className={styles.galleryContainer}>
+        <TestCardClient title="test1" />
+        <TestCardClient title="test2" />
+        <TestCardClient title="test3" />
+        <TestCardClient title="test4" />
+        <TestCardClient title="test5" />
+      </div>
+    </div>
+  );
 }
 
 export default test_selection;
