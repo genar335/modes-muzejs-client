ad2c5f8420117a8d49516a9cba3e98b111e8e436
Daniel <daniel.voronezhskiy@gmail.com>
2020-12-14 11:56:11 +0200

    next files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,11 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
+<<<<<<< Updated upstream
 /******/ 	var hotCurrentHash = "fb6b7bc0fa4df66d95a1";
+=======
+/******/ 	var hotCurrentHash = "5f31602ae0ad9279e14a";
+>>>>>>> Stashed changes
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
