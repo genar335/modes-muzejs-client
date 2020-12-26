355947bb921ded176e38bc01c85d4f4d857b275e
genar335 <domvlielupe3@gmail.com>
2020-12-26 18:24:53 +0200

    client_test; Added answer cards shuffling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "13566d77fb375d31b3af";
+/******/ 	var hotCurrentHash = "f04e80737d7daa10c773";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
