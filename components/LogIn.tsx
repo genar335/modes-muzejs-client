5a523f4e48ebe31e3b69c8379d3180c512df14a6
Vadims Seinins <seinins.v@gmail.com>
2021-05-04 19:11:55 +0300

    Console logged the repsonse from login

--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -35,6 +35,7 @@ const LogIn = () => {
         }
       );
       console.log(response.status);
+      console.log(response);
       await Router.push("/");
       //setIsLoading(false);
     } catch (error) {
