156b30384989ae26fb2bd657d27c5253634a6267
Vadims Seinins <seinins.v@gmail.com>
2021-05-22 00:09:59 +0100

    Added one more condition for the check of JWT

--- components/jwtChecker.tsx
+++ components/jwtChecker.tsx
@@ -3,6 +3,6 @@ import store from "store";
 const checkJWT = (): boolean => {
   //   console.log(store.get("jwt"));
   //   alert(store.get("jwt") == undefined);
-  return store.get("jwt") == undefined ? false : true;
+  return store.get("jwt") == undefined && store.get("jwt") == "" ? false : true;
 };
 export default checkJWT;
