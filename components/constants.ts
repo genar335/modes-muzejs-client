34d8ce130ad463efbde42ba561fef590cc72020a
genar335 <domvlielupe3@gmail.com>
2021-07-13 18:08:02 +0300

    Fixed typo in a URL

--- components/constants.ts
+++ components/constants.ts
@@ -4,7 +4,7 @@ export const white: string = "255 255 255";
 // export const APIURL: string = "http://192.168.8.100:4000/";
 export const APIURL: string = "http://localhost:4000/";
 // export const productionURL: string = 'https://modesmuzejs.lv/api/quiz/'
-export const productionURL: string = "https://fashionmuseumriga.lv/api/quiz";
+export const productionURL: string = "https://fashionmuseumriga.lv/api/quiz/";
 export const productionHost: string =
   "https://vigilant-torvalds-39724e.netlify.app";
 // export const devURL: string = "http://192.168.8.100:3000/";
