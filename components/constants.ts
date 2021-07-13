f46cddac16580162e91b43b64426f47461b97087
genar335 <domvlielupe3@gmail.com>
2021-07-13 17:56:17 +0300

    Updated prod URL

--- components/constants.ts
+++ components/constants.ts
@@ -2,11 +2,13 @@ export const brown: string = "236 221 210";
 export const white: string = "255 255 255";
 //* Host adressess
 // export const APIURL: string = "http://192.168.8.100:4000/";
-export const APIURL: string = "http://localhost:4000/"
-export const productionURL: string = 'https://modesmuzejs.lv/api/quiz/'
-export const productionHost: string = 'https://vigilant-torvalds-39724e.netlify.app'
+export const APIURL: string = "http://localhost:4000/";
+// export const productionURL: string = 'https://modesmuzejs.lv/api/quiz/'
+export const productionURL: string = "http://fashionmuseumriga.lv/api/quiz";
+export const productionHost: string =
+  "https://vigilant-torvalds-39724e.netlify.app";
 // export const devURL: string = "http://192.168.8.100:3000/";
-export const devURL: string = 'http://localhost:3000/'
+export const devURL: string = "http://localhost:3000/";
 //* Carousel settings for responsivnes
 export const responsive = {
   superLargeDesktop: {
@@ -28,8 +30,10 @@ export const responsive = {
   },
 };
 //* Regex checks
-export const URLCheck: RegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-export const URLCheckForLocalHost: RegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}/;
+export const URLCheck: RegExp =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+export const URLCheckForLocalHost: RegExp =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}/;
 
 //* Drag n Drop related
 export const ItemTypes = {
