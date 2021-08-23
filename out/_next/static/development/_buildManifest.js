df0a266c55910b3cfadbd08a474effd48af67064
genar335 <domvlielupe3@gmail.com>
2021-08-23 12:36:09 +0300

    Testing...

---
+++ out/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/TMS/auth":["static\u002Fchunks\u002Fpages\u002FTMS\u002Fauth.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002FTMS\u002Fauth","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
