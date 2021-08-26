eb44c737fa943a4ac21409cecf771c49c37746bf
genar335 <domvlielupe3@gmail.com>
2021-08-26 15:41:07 +0300

    Added meta tag to enbale http access

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,27 +4,37 @@ import dynamic from "next/dynamic";
 import "react-multi-carousel/lib/styles.css";
 import { AnimateSharedLayout, motion } from "framer-motion";
 import React from "react";
+import Head from "next/head";
 
 function App({ Component, pageProps, router }: AppProps) {
   return (
-    <motion.div
-      initial="pageInitial"
-      animate="pageAnimate"
-      variants={{
-        pageInitial: {
-          // x: -1000,
-          opacity: 0,
-        },
-        pageAnimate: {
-          x: 0,
-          opacity: 1,
-        },
-      }}
-      key={router.route}
-    >
-      {" "}
-      <Component {...pageProps} />{" "}
-    </motion.div>
+    <div>
+      <Head>
+        <title>FM API</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <motion.div
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            // x: -1000,
+            opacity: 0,
+          },
+          pageAnimate: {
+            x: 0,
+            opacity: 1,
+          },
+        }}
+        key={router.route}
+      >
+        {" "}
+        <Component {...pageProps} />{" "}
+      </motion.div>
+    </div>
   );
 }
 
