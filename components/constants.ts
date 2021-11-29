e9d6dbfd9b105301a1d9ac04c9a94bd9afd362ba
Vadim <seinins.v@gmail.com>
2021-11-29 20:31:10 +0200

    Swapped localhost with current DO droplet's IP

--- components/constants.ts
+++ components/constants.ts
@@ -6,41 +6,41 @@ export const APIURL: string = "http://localhost:4000/";
 // export const productionURL: string = 'https://modesmuzejs.lv/api/quiz/'
 // export const productionURL: string = "https://fashionmuseumriga.lv/api/quiz/";
 // export const productionURL = "http://167.99.250.21/api/quiz/";
-// export const productionURL = "http://68.183.213.125/api/quiz/";
-export const productionURL = "http://localhost:4000/api/quiz/";
+export const productionURL = "http://68.183.213.125/api/quiz/";
+// export const productionURL = "http://localhost:4000/api/quiz/";
 export const productionHost: string =
-  "https://vigilant-torvalds-39724e.netlify.app";
+    "https://vigilant-torvalds-39724e.netlify.app";
 // export const devURL: string = "http://192.168.8.100:3000/";
 export const devURL: string = "http://localhost:3000/";
 //* Carousel settings for responsivnes
 export const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: {max: 4000, min: 3000},
+        items: 5,
+    },
+    desktop: {
+        breakpoint: {max: 3000, min: 1024},
+        items: 3,
+    },
+    tablet: {
+        breakpoint: {max: 1024, min: 464},
+        items: 2,
+    },
+    mobile: {
+        breakpoint: {max: 464, min: 0},
+        items: 1,
+    },
 };
 //* Regex checks
 export const URLCheck: RegExp =
-  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 export const URLCheckForLocalHost: RegExp =
-  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}/;
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}/;
 
 //* Drag n Drop related
 export const ItemTypes = {
-  QCARD: "qcard",
+    QCARD: "qcard",
 };
 
 //* Max input length numbers
