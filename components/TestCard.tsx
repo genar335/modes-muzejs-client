a2a86968123ea58cdc94306f8442564289d22f82
Vadim <seinins.v@gmail.com>
2021-12-11 17:31:09 +0200

    Renamed TMS to tms

--- components/TestCard.tsx
+++ components/TestCard.tsx
@@ -33,7 +33,7 @@ const TestCard = (props: {
 
     const router: NextRouter = useRouter();
     const forwardToTestEdit = (testID: string) => {
-        router.push(`/TMS/create_test?testToEdit=${testID}`);
+        router.push(`/tms/create_test?testToEdit=${testID}`);
     };
 
     // console.log(props, "card props");
